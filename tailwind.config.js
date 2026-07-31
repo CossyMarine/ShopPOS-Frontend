d97ec4feb3adfd7467025ddf5620180a15606765
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-01 00:36:09 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          orange: '#FF5722',
+          'orange-hover': '#E64A19',
+          'orange-light': '#FFF0EB',
+          dark: '#0A0A0A',
+          gray: '#64748B',
+        },
+      },
+    },
   },
-  plugins: [], // This must be an array
+  plugins: [],
 }
