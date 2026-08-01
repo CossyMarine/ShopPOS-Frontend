937849dc16b30952a00b5a0e7bccb2da408cf77e
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-01 16:23:58 +0300

    Create Field.jsx

---
+++ src/components/Storekeeper/Field.jsx
@@ -0,0 +1,8 @@
+export default function Field({ label, children }) {
+    return (
+        <div>
+            <label className="block text-xs font-semibold text-gray-400 mb-1">{label}</label>
+            {children}
+        </div>
+    );
+}
