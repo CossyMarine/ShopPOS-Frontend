ac0fbaa932a55422994f6747038b3be4ce72a562
CossyMarine <shidayawatukibwari@gmail.com>
2026-08-25 01:00:07 +0300

    Update productManagementUtils.js

--- src/components/Storekeeper/productManagementUtils.js
+++ src/components/Storekeeper/productManagementUtils.js
@@ -23,6 +23,7 @@ export const EMPTY_FORM = {
     isBulk: false, packSize: '10', caseLabel: 'Carton', caseBarcode: '', casePrice: '',
     imageUrl: '', imagePublicId: '',
     openingQty: '', openingCost: '', openingReceivedAs: 'each',
+    vatClass: 'standard',
 };
 export const EMPTY_STOCK = { quantity: '', costPerUnit: '', expiryDate: '', supplierNote: '', receivedAs: 'each' };
 export const EMPTY_UNIT = { name: '', abbreviation: '' };
